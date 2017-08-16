49e61e3938c8afc2bb84bd0dbaf0ecde3bdfdfe0
System <none>
2017-08-16 05:58:22 +0000

    Checkpoint

---
+++ config/database.js
@@ -0,0 +1,5 @@
+module.exports = {
+
+    'url' : process.env.DBURL || 'your-settings-here' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
--- server.js
+++ server.js
@@ -19,7 +19,7 @@ var session      = require('express-session');
 var configDB = require('./config/database.js');
 
 // configuration ===============================================================
-mongoose.connect(configDB.url); // connect to our database
+//mongoose.connect(configDB.url); // connect to our database
 
 // require('./config/passport')(passport); // pass passport for configuration
 
@@ -35,7 +35,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(session({
     secret: 'thisismysecretthisismysecret2thisismysecret',
     resave: false,
-    saveUninitialized: true
+    saveUninitialized: false
 }));
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
