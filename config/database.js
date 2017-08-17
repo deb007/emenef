0da4e572572e8a8eb0c0ca97158579748798d370
System <none>
2017-08-17 05:54:29 +0000

    Checkpoint

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
 
-    'url' : "mongodb://"
+    'url' : "mongodb://admin:thisisago0dpassword@cluster0-shard-00-00-3ryul.mongodb.net:27017,cluster0-shard-00-01-3ryul.mongodb.net:27017,cluster0-shard-00-02-3ryul.mongodb.net:27017/Cluster1?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin"
 
 };
