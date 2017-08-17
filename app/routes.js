564ea7e5e814ba0779c50f8490d90f39101e3f91
System <none>
2017-08-17 04:37:29 +0000

    Checkpoint

--- app/routes.js
+++ app/routes.js
@@ -1,14 +1,14 @@
 module.exports = function(app, passport) {
 
     app.get('/', function(req, res) {
-        res.render('../views/index'); // load the index.ejs file
+        res.render('../views/index', {APP_TITLE: process.env.APP_TITLE}); // load the index.ejs file
     });
 
     // show the login form
     app.get('/login', function(req, res) {
 
         // render the page and pass in any flash data if it exists
-        res.render('../views/login', { message: req.flash('loginMessage') }); 
+        res.render('../views/login', { message: req.flash('loginMessage'), APP_TITLE: process.env.APP_TITLE }); 
     });
 
     // process the login form
