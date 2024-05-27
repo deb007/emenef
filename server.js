3f259869d7eb193f90ddf9e31d2d05ff06703b5f
Glitch (emenef) <none>
2024-05-27 12:34:13 +0000

    ⚓️👜 Checkpoint ./server.js:241333/536

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ models.sequelize.sync().then(function() {
 }).catch(function(err) {
     console.log(err, "Something went wrong with the Database Update!");
 });
+console.log(models.user);
 process.exit();
 
 require('./config/passport')(passport, models.user); // pass passport for configuration
